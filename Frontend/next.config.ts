472d6c913d7e371a28f7d595d1f69fad474d0f65
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-11-18 17:21:10 +0100

    Feat: deploy settings

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,27 +1,26 @@
+// ESTRUCTURA CORRECTA
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // La función headers es una propiedad
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: '/:path*',
         headers: [
           {
-            key: "Content-Security-Policy",
-            // Añadimos 'unpkg.com' a la lista de fuentes de scripts permitidas
-            value:
-              "worker-src 'self' blob: https://unpkg.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://unpkg.com;",
+            key: 'Content-Security-Policy',
+            value: "worker-src 'self' blob: https://unpkg.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://unpkg.com;",
           },
         ],
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        webpack: (config: { externals: string[]; }, { isServer }: any) => {
-          // Añade una regla para tratar 'canvas' como un módulo externo
-          config.externals.push("canvas");
-          return config;
-        },
       },
     ];
+  },
+
+  // La función webpack es OTRA propiedad, al mismo nivel que headers
+  webpack: (config) => {
+    config.externals.push('canvas');
+    return config;
   },
 };
 
