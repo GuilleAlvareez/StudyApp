47f21a9f32fc7965d493ed0e92bbafc378a61cea
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-11-18 17:17:08 +0100

    Feat: deploy settings

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -5,14 +5,21 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         headers: [
           {
-            key: 'Content-Security-Policy',
+            key: "Content-Security-Policy",
             // Añadimos 'unpkg.com' a la lista de fuentes de scripts permitidas
-            value: "worker-src 'self' blob: https://unpkg.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://unpkg.com;",
+            value:
+              "worker-src 'self' blob: https://unpkg.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://unpkg.com;",
           },
         ],
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        webpack: (config: { externals: string[]; }, { isServer }: any) => {
+          // Añade una regla para tratar 'canvas' como un módulo externo
+          config.externals.push("canvas");
+          return config;
+        },
       },
     ];
   },
